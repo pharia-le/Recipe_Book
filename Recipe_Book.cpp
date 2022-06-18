d6c98bda48e68a708034e0f576a01aaf0ca13b41
Pharia Le <pharia.le@gmail.com>
2022-06-18 12:35:23 -0700

    Add initial cpp file with main()
    Adding bare cpp file with bare
    bones main()

---
+++ Recipe_Book.cpp
@@ -0,0 +1,16 @@
+/*
+	Recipe Book
+	A small project to store & retrieve recipes.
+
+	Pharia Le
+	18 June 2022
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Welcome to Reipe Book! :)" << endl;
+}
